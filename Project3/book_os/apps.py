e78b0584c23fcf65b9b4289c883d62854fed9981
meleuo <meleuo@outlook.com>
2020-12-07 17:20:31 +0800

    已完成注册登录功能

--- Project3/book_os/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookOsConfig(AppConfig):
-    name = 'book_os'
--- ajax_demo/app01/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class UserDB(models.Model):
-    user = models.CharField(max_length=10)
