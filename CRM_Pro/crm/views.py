acf489bcf7ba314d7ae0e5048a3ea4e7124d1900
meleuo <meleuo@outlook.com>
2020-12-07 17:07:31 +0800

    已完成注册登录功能

--- CRM_Pro/crm/views.py
+++ CRM_Pro/crm/views.py
@@ -3,11 +3,29 @@ from django.views.generic import View
 # Create your views here.
 
 from django.contrib import auth
+from crm.forms import RegisterForm
+from crm import models
 
 
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'register.html')
+        form_obj = RegisterForm()
+        return render(request, 'register.html', {'form_obj': form_obj})
+
+    def post(self, request):
+        form_obj = RegisterForm(request.POST)
+        if not form_obj.is_valid():
+            return render(request, 'register.html', {'form_obj': form_obj})
+
+        '''
+        form_obj.cleaned_data.pop('re_password')
+        models.UserProfile.objects.create_user(**form_obj.cleaned_data)
+        '''
+        new_user = form_obj.save()
+        new_user.set_password(new_user.password)
+        new_user.save()
+        return redirect('/login/')
+
 
 class LoginView(View):
     def get(self, request):
