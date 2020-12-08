32c03ef709acfcbda2aa8e391c86369fdff08c7c
meleuo <meleuo@outlook.com>
2020-12-08 15:30:54 +0800

    分页器完成

--- crm/views.py
+++ crm/views.py
@@ -1,35 +1,89 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
-# Create your views here.
-
+from django.utils.safestring import mark_safe
 from django.contrib import auth
+
 from crm import models
 from crm.forms import RegisterForm
 from crm import models
+from utils.pagination import Pagination
 
 
 class Customer_list(View):
-
     def get(self, request):
-        per_num = 30
-        page_num = request.GET.get('page', 1)
-        if page_num.isdigit():
-            page_num = int(page_num)
-        else:
-            page_num = 1
-
-        #--> 获取数据的起始和结束值
-        start_num = (page_num - 1)*per_num
-        stop_num = page_num * per_num
-
-        #--> 获取页数
         all_customer = models.Customer.objects.all()
-        all_count, more = divmod(len(all_customer), 30)
-        if more:
-            all_count = all_count + 1
+        page_num = request.GET.get('page', 1)
 
-        return render(request, 'crm/customer_list.html',
-                      {'all_customer': all_customer[start_num:stop_num], 'all_count': range(1, all_count + 1), 'page_num': page_num})
+        pagination_obj = Pagination(page_num=page_num, len_customer=len(all_customer), url=reverse('customer_list'))
+
+        return render(request, 'crm/customer_list.html', {
+            'all_customer': all_customer[pagination_obj.start:pagination_obj.end],
+            'html_page': mark_safe(pagination_obj.show_html)
+        })
+
+
+# class Customer_list(View):
+#
+#     def get(self, request):
+#         page_num = request.GET.get('page', 1)
+#         try:
+#             page_num = int(page_num)
+#             if page_num <= 0:
+#                 page_num = 1
+#         except Exception as e:
+#             page_num = 1
+#
+#         per_num = 10  # --> 一页显示数据条数
+#         start_num = (page_num - 1) * per_num
+#         stop_num = page_num * per_num
+#
+#         # --> 获取页数
+#         all_customer = models.Customer.objects.all()
+#         all_count, more = divmod(len(all_customer), per_num)
+#         if more:
+#             all_count = all_count + 1
+#
+#         max_show = 10  # --> 最多显示的页码数
+#         half_show = max_show // 2  # --> 取得两边分页的长度, // 向下取值
+#
+#         if page_num <= half_show:  # -->  倘若请求的页面小于2边分页的长度
+#             page_start = 1  # --> 分页起始值写死为1
+#             page_end = max_show + 1  # --> 分页结束值设置为最大值, 再加上当前页面的分页, 一共是11个分页
+#         elif page_num >= all_count - half_show:  # --> 倘若请求的页面大于  (全部页面数 - 分页数)
+#             page_start = all_count - max_show  # --> 起始页面 设置为 页面总数 - 最大长度
+#             page_end = all_count  # --> 结尾设置总数即可
+#         else:  # --> 其他正常情况保证当前分页的两侧有对应的数量的分页即可
+#             page_start = page_num - half_show
+#             page_end = page_num + half_show
+#
+#         # --> 生成HTML
+#         html_page = ''
+#         if page_num > 1:
+#             html_page = '<li><a href="{url}?page=1" aria-label="Previous"><span aria-hidden="true">首页</span></a></li>' \
+#                         '<li><a href="{url}?page={count}" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>' \
+#                 .format(url=reverse('customer_list'), count=page_num - 1)
+#         else:
+#             html_page += '<li class="disabled"><span aria-hidden="true">首页</span></li><li class="disabled"><span aria-hidden="true">&laquo;</span></li>'
+#
+#
+#         for count in range(page_start, page_end + 1):
+#             page_is_active = ''
+#             if page_num == count:
+#                 page_is_active = 'class="active"'
+#             html_page += '<li {active}><a href="{url}?page={count}">{count}</a></li>' \
+#                 .format(active=page_is_active, url=reverse('customer_list'), count=count)
+#
+#         if page_num < all_count:
+#             html_page += '<li><a href="{url}?page={count}" aria-label="Previous"><span aria-hidden="true">&raquo;</span></a></li>' \
+#                          '<li><a href="{url}?page={all_count}" aria-label="Previous"><span aria-hidden="true">尾页</span></a></li>' \
+#                 .format(url=reverse('customer_list'), count=page_num + 1, all_count=all_count)
+#         else:
+#             html_page += '<li class="disabled"><span aria-hidden="true">&raquo;</span></li><li class="disabled"><span aria-hidden="true">尾页</span></li>'
+#
+#         return render(request, 'crm/customer_list.html', {
+#             'all_customer': all_customer[start_num:stop_num],
+#             'html_page': mark_safe(html_page)
+#         })
 
 
 class RegisterView(View):
---
+++ utils/pagination.py
@@ -0,0 +1,68 @@
+# --> 分页器方法
+
+class Pagination():
+    def __init__(self, page_num, len_customer, url, max_show=10, per_num=10):
+        try:
+            self.page_num = int(page_num)  # --> 当前页面的 序号
+            if self.page_num <= 0:
+                self.page_num = 1
+        except Exception as e:
+            self.page_num = 1
+
+        self.url = url  # --> 页面的URl
+        self.max_show = max_show  # --> 最多显示的页码数
+        self.per_num = per_num  # --> 一页显示数据条数
+
+        self.all_count, more = divmod(len_customer, self.per_num)
+        if more:
+            self.all_count = self.all_count + 1
+
+        half_show = self.max_show // 2  # --> 取得两边分页的长度, // 向下取值
+
+        if self.page_num <= half_show:  # -->  倘若请求的页面小于2边分页的长度
+            self.page_start = 1  # --> 分页起始值写死为1
+            self.page_end = self.max_show + 1  # --> 分页结束值设置为最大值, 再加上当前页面的分页, 一共是11个分页
+
+        elif self.page_num >= self.all_count - half_show:  # --> 倘若请求的页面大于  (全部页面数 - 分页数)
+            self.page_start = self.all_count - self.max_show  # --> 起始页面 设置为 页面总数 - 最大长度
+            self.page_end = self.all_count  # --> 结尾设置总数即可
+
+        else:  # --> 其他正常情况保证当前分页的两侧有对应的数量的分页即可
+            self.page_start = self.page_num - half_show
+            self.page_end = self.page_num + half_show
+    @property
+    def start(self):
+        return (self.page_num - 1) * self.per_num
+
+    @property
+    def end(self):
+        return self.page_num * self.per_num
+
+    @property
+    def show_html(self):
+        # --> 生成HTML
+        self.html_page = ''
+        print(self.page_num)
+        if self.page_num > 1:
+            self.html_page = '<li><a href="{url}?page=1" aria-label="Previous"><span aria-hidden="true">首页</span></a></li>' \
+                             '<li><a href="{url}?page={count}" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>' \
+                .format(url=self.url, count=self.page_num - 1)
+
+        else:
+            self.html_page += '<li class="disabled"><span aria-hidden="true">首页</span></li><li class="disabled"><span aria-hidden="true">&laquo;</span></li>'
+
+        for count in range(self.page_start, self.page_end + 1):
+            page_is_active = ''
+            if self.page_num == count:
+                page_is_active = 'class="active"'
+            self.html_page += '<li {active}><a href="{url}?page={count}">{count}</a></li>' \
+                .format(active=page_is_active, url=self.url, count=count)
+
+        if self.page_num < self.all_count:
+            self.html_page += '<li><a href="{url}?page={count}" aria-label="Previous"><span aria-hidden="true">&raquo;</span></a></li>' \
+                              '<li><a href="{url}?page={all_count}" aria-label="Previous"><span aria-hidden="true">尾页</span></a></li>' \
+                .format(url=self.url, count=self.page_num + 1, all_count=self.all_count)
+        else:
+            self.html_page += '<li class="disabled"><span aria-hidden="true">&raquo;</span></li><li class="disabled"><span aria-hidden="true">尾页</span></li>'
+
+        return self.html_page
